140c1851e4f42967f4688b803d6c86df5ce02b4b
tamarabernad <tamara@codedazur.es>
2014-05-27 16:43:41 +0200

    Initial Commit

---
+++ sing-the-gap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sing-the-gap
+//
+//  Created by Tamara Bernad on 27/05/14.
+//  Copyright (c) 2014 Tamara Bernad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBWAppDelegate class]));
+    }
+}
